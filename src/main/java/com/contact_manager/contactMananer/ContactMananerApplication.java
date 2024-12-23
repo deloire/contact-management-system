a7940d4702e76dca19b8d1a05c9783a7de1b62e9
deloire <sashamax098760@gmail.com>
2024-12-23 18:45:36 +0300

    Initial commit

---
+++ src/main/java/com/contact_manager/contactMananer/ContactMananerApplication.java
@@ -0,0 +1,13 @@
+package com.contact_manager.contactMananer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMananerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMananerApplication.class, args);
+	}
+
+}
